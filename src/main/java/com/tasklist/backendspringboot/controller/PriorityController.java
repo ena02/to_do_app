70ebae7066549f4da8ee412b989523d04ae6544a
ena02 <erkembekov.nurzhan@mail.ru>
2022-03-16 10:29:54 +0600

    I added feature "add" to controllers

--- src/main/java/com/tasklist/backendspringboot/controller/PriorityController.java
+++ src/main/java/com/tasklist/backendspringboot/controller/PriorityController.java
@@ -2,6 +2,8 @@ package com.tasklist.backendspringboot.controller;
 
 import com.tasklist.backendspringboot.entity.Priority;
 import com.tasklist.backendspringboot.repo.PriorityRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -26,7 +28,35 @@ public class PriorityController {
     }
 
     @PostMapping("/add")
-    public Priority add(@RequestBody Priority priority) {
-        return priorityRepository.save(priority);
+    public ResponseEntity<Priority> add(@RequestBody Priority priority) {
+
+        if (priority.getId() != null && priority.getId() != 0) {
+            return new ResponseEntity("redundant param: id MUST be null", HttpStatus.NOT_ACCEPTABLE);
+        }
+
+        if (priority.getTitle() == null || priority.getTitle().trim().length() == 0) {
+            return new ResponseEntity("missed param: title", HttpStatus.NOT_ACCEPTABLE);
+        }
+
+
+        return ResponseEntity.ok(priorityRepository.save(priority));
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity<Priority> update(@RequestBody Priority priority) {
+
+        if (priority.getId() == null && priority.getId() == 0) {
+            return new ResponseEntity("id can not be null", HttpStatus.NOT_ACCEPTABLE);
+        }
+
+        if (priority.getTitle() == null || priority.getTitle().trim().length() == 0) {
+            return new ResponseEntity("missed param: title", HttpStatus.NOT_ACCEPTABLE);
+        }
+
+        if (priority.getColor() == null || priority.getColor().trim().length() == 0) {
+            return new ResponseEntity("missed param: color", HttpStatus.NOT_ACCEPTABLE);
+        }
+
+        return ResponseEntity.ok(priorityRepository.save(priority));
     }
 }
