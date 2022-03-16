c911f8422d2e79d99fb3a159b0cd46150f8910ee
ena02 <erkembekov.nurzhan@mail.ru>
2022-03-16 10:32:18 +0600

    Revert "I added feature "add" to controllers"
    This reverts commit 70ebae7066549f4da8ee412b989523d04ae6544a.

--- src/main/java/com/tasklist/backendspringboot/controller/CategoryController.java
+++ src/main/java/com/tasklist/backendspringboot/controller/CategoryController.java
@@ -2,8 +2,6 @@ package com.tasklist.backendspringboot.controller;
 
 import com.tasklist.backendspringboot.entity.Category;
 import com.tasklist.backendspringboot.repo.CategoryRepository;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -25,31 +23,7 @@ public class CategoryController {
     }
 
     @PostMapping("/add")
-    public ResponseEntity<Category> add(@RequestBody Category category) {
-
-        if (category.getId() != null && category.getId() != 0) {
-            return new ResponseEntity("redundant param: id MUST be null", HttpStatus.NOT_ACCEPTABLE);
-        }
-
-        if (category.getTitle() == null || category.getTitle().trim().length() == 0) {
-            return new ResponseEntity("missed param: title", HttpStatus.NOT_ACCEPTABLE);
-        }
-
-        return ResponseEntity.ok(categoryRepository.save(category));
-    }
-
-    @PutMapping("/update")
-    public ResponseEntity<Category> update(@RequestBody Category category) {
-
-        if (category.getId() == null && category.getId() == 0) {
-            return new ResponseEntity("id can not be null", HttpStatus.NOT_ACCEPTABLE);
-        }
-
-
-        if (category.getTitle() == null || category.getTitle().trim().length() == 0) {
-            return new ResponseEntity("missed param: title", HttpStatus.NOT_ACCEPTABLE);
-        }
-
-        return ResponseEntity.ok(categoryRepository.save(category));
+    public Category add(@RequestBody Category category) {
+        return categoryRepository.save(category);
     }
 }
